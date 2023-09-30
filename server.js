a633beb0bbd2c8caaab652ae5cc16610fc4479e0
arsenyeganyan <125376725+arsenyeganyan@users.noreply.github.com>
2023-09-30 18:12:13 +0400

    Fixing the server side

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ dotenv.config();
 app.use('/', express.static(path.join(__dirname, './client/dist')));
 app.use(bodyParser.urlencoded({ extended: false }));
 //Errors
-app.use((req, res) => {
+app.get((req, res) => {
     res.status(404).sendFile(path.join(__dirname, '/error.html'));
 });
 
